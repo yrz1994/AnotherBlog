c5fd5d1df8fb6911008d5ee0db5d79eed9968862
Yang, Edward <yrz1994@gmail.com>
2021-03-11 18:14:57 +0800

    Add memorybus.

---
+++ AnotherBlog.Domain.Core/Bus/InMemoryBus.cs
@@ -0,0 +1,28 @@
+﻿using AnotherBlog.Domain.Core.Messages.Commands;
+using AnotherBlog.Domain.Core.Messages.Events;
+using FluentValidation.Results;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace AnotherBlog.Domain.Core.Bus
+{
+    public class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task PublishEvent<T>(T @event) where T : Event
+        {
+            await _mediator.Publish(@event);
+        }
+
+        public async Task<ValidationResult> SendCommand<T>(T command) where T : Command
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
